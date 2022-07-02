55542dd299771e0c8f62507c1bde2d0955081175
Dylan Fawkes <dylanfawkes93@gmail.com>
2022-07-03 00:11:38 +0800

    Disable pulseaudio because it was causing fires

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -136,14 +136,14 @@ datewidget = wibox.widget.textbox()
 vicious.register(datewidget, vicious.widgets.date, " %b %d, %R")
 
 -- Create a volume widget
-vol = wibox.widget.textbox()
-local sink = "alsa_output.usb-Burr-Brown_Japan_Burr-Brown_Japan_PCM2702-00.iec958-stereo"
-vicious.register(vol, vicious.contrib.pulse, "墳 $1%", 2, sink)
-vol:buttons(awful.util.table.join(
-    awful.button({}, 1, function () vicious.contrib.pulse.toggle(sink) end),
-    awful.button({}, 2, function () awful.util.spawn("pavucontrol") end),
-    awful.button({}, 4, function () vicious.contrib.pulse.add(5, sink) end),
-    awful.button({}, 5, function () vicious.contrib.pulse.add(-5, sink) end)))
+vol = wibox.widget.textbox("墳")
+-- local sink = "alsa_output.usb-Burr-Brown_Japan_Burr-Brown_Japan_PCM2702-00.iec958-stereo"
+-- vicious.register(vol, vicious.contrib.pulse, "墳 $1%", 2, sink)
+-- vol:buttons(awful.util.table.join(
+    -- awful.button({}, 1, function () vicious.contrib.pulse.toggle(sink) end),
+    -- awful.button({}, 2, function () awful.util.spawn("pavucontrol") end),
+    -- awful.button({}, 4, function () vicious.contrib.pulse.add(5, sink) end),
+    -- awful.button({}, 5, function () vicious.contrib.pulse.add(-5, sink) end)))
 
 -- RAM widget
 ramw = wibox.widget.textbox()
