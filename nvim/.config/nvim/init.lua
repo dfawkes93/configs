42b6cf39e0547321ebbf2b86a76369fd1fb2d421
Dylan Fawkes <dylan.fawkes@21cs.com>
2023-11-25 21:17:21 +0800

    Add neorg

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -70,6 +70,28 @@ require('lazy').setup({
     },
   },
 
+  -- Neorg,
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},    -- Loads default behaviour
+          ["core.concealer"] = {},   -- Adds pretty icons to your documents
+          ["core.dirman"] = {        -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',   opts = {} },
   {
@@ -272,7 +294,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 
 -- Toggleterm Map
 function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
+  local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
@@ -280,6 +302,7 @@ function _G.set_terminal_keymaps()
   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
+
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 vim.keymap.set('n', "<leader>xx", ":ToggleTermSendCurrentLine<CR>")
 vim.keymap.set('n', "<C-t>", ":ToggleTerm<CR>")
